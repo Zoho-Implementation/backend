6b78b9fabd9960917a6b005a540f1f2ccd4df821
Mykyta Shelest <shelestmykyta2022@gmail.com>
2023-05-06 23:26:13 +0300

    implemented http query builder

---
+++ app/Helpers/Constants.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Helpers;
+
+class Constants
+{
+    // Methods
+    const METHOD_GET = 'GET';
+    // Codes
+    const INTERNAL_SERVER_ERROR = 500;
+    // Messages
+    const SOMETHING_WENT_WRONG_MESSAGE = 'something went wrong';
+}
