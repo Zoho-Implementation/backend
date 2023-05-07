4abfc117b1d9cf0175aae11b0c05b1c954fab100
Mykyta Shelest <shelestmykyta2022@gmail.com>
2023-05-07 23:14:41 +0300

    add mysql + make migrate

---
+++ app/Modules/Token/Token.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Modules\Token;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Token extends Model
+{
+    use HasFactory;
+}
