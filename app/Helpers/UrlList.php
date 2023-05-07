bde229e7b077ab865ab29209a04fed46a1fee1bc
Mykyta Shelest <shelestmykyta2022@gmail.com>
2023-05-07 21:22:24 +0300

    create AccessTokenService

---
+++ app/Helpers/UrlList.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Helpers;
+
+class UrlList
+{
+    const GENERATE_ACCESS_TOKEN = '/oauth/v2/token';
+    const REFRESH_ACCESS_TOKEN = '/oauth/v2/token';
+    const CREATE_ACCOUNT = '/crm/v2/Accounts';
+}
---
+++ app/Modules/Account/Controllers/AccountController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Modules\Account\Controllers;
+
+use App\Helpers\Constants;
+use App\Helpers\UrlList;
+use App\Modules\Account\Requests\AccountRequest;
+
+class AccountController extends Constants
+{
+    public function create(AccountRequest $request)
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+}
